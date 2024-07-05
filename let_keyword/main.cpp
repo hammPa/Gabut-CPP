0ccac8d74dc74681163f3b531309347390591168
hammPa <121952935+hammPa@users.noreply.github.com>
2024-07-05 13:59:36 +0700

    Create main.cpp

---
+++ let_keyword/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "let.hpp"
+
+int main(int argc, char const *argv[]){
+    
+    let::fitur::tampil();
+
+    // let angka;
+    // std::cout << "Masukkan angka : ";
+    // std::cin >> angka;
+    // std::cout << "Angka : " << angka << "\n";
+
+
+
+    return 0;
+}
+
+    // // variabel dasar
+    // let numInt = 10;
+    // let numFloat = 10.5f;
+    // let numDouble = 10.3444444444444;
+    // let numChar = 'a';
+    // let numString = "oila";
+
+
+    // std::cout << "numInt: " << numInt << std::endl;
+    // std::cout << "numFloat: " << numFloat << std::endl;
+    // std::cout << "numDouble: " << numDouble << std::endl;
+    // std::cout << "numChar: " << numChar << std::endl;
+    // std::cout << "numString: " << numString << std::endl;
+
+    // // maematika
+    // let a = 10;
+    // let b = 40.5f;
+    // let tambah = a + b;
+    // let kurang = a - b;
+    // let kali = a * b;
+    // let bagi = a / b;
+    // std::cout << "+ : " << tambah << std::endl;
+    // std::cout << "- : " << kurang << std::endl;
+    // std::cout << "* : " << kali << std::endl;
+    // std::cout << "/ : " << bagi << std::endl;
+
+
+
+    // // assignment
+    // let x = 10;
+    // let y = x;
+    // y = 40;
+    // std::cout << x << std::endl;
+
+    // // reassign
+    // let misal = 10;
+    // std::cout << misal << std::endl;
+    // misal = "apala";
+    // std::cout << misal << std::endl;
+
+    // // int *misalnya = (int[]){10,40};
+
+    // let arr = {10, 20};
+
+    // std::cout << arr << std::endl;
+    // std::cout << "arr[0] : " << arr[0] << std::endl;
+    // std::cout << "arr[1] : " << arr[1] << std::endl;
+    // std::cout << "arr[2] : " << arr[2] << std::endl;
+
+
+
+
+
+
+
+
+
+    // let array = {10, 5.5f, 32432.234, 'c', "tekss"};
+    // std::cout << array << "\n";
+    // std::cout << array[0] << "\n";
+    // std::cout << array[1] << "\n";
+    // std::cout << array[2] << "\n";
+    // std::cout << array[3] << "\n";
+    // std::cout << array[4] << "\n";
+
+    // array.push_back(99);
+    // std::cout << array[5] << "\n";
+    // std::cout << array.size() << "\n";
+
+
+    // let a = 10;
+    // let b = 10;
+    // let arr1 = {10, 20, 30};
+    // let arr2 = {10, 20, 40};
+    // let ril = false;
+    // std::cout << ril << "\n";
+    // std::cout << (a == b) << "\n";
+    // std::cout << (arr1 == arr2) << "\n";
+    // std::cout << (arr1 != arr2) << "\n";
